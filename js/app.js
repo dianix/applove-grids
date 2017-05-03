22f00d8ae148eaeaef1126e9521c690340fa7815
dianix <dianavazquez.dcg@gmail.com>
2017-05-03 15:28:46 -0500

    terminadas secciones, agregando funcionalidad

--- js/app.js
+++ js/app.js
@@ -1,3 +1,27 @@
-/*
- * Archivo principal de funcionalidad de JS
- */
+//Archivo principal de funcionalidad de JS
+
+var modal = document.getElementById("modalArea");
+var imagenModal = document.getElementById("imagenVentana");
+var boton = document.getElementById("close");
+var fotos = document.getElementsByClassName("fotito");
+
+function detectarImagen() {
+    mostrarModal(this);
+    console.log(this);
+}
+
+function mostrarModal(imagen){
+    console.log(modal);
+    modal.style.display = "block";
+    imagenModal.src = imagen.src;
+}
+
+function cerrarModal(){
+    modal.style.display = "none";
+}
+
+for(var i = 0; i < fotos.length; i++){
+    fotos[i].addEventListener("click",detectarImagen);
+}
+
+boton.addEventListener("click",cerrarModal);
